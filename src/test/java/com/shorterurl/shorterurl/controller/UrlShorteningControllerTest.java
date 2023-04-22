6611a2a8198c6167c3a628f63e8ceba1bf375a79
simiyu1 <nabendesimiyu@gmail.com>
2023-04-22 14:15:37 +0300

    add tests

--- src/test/java/com/shorterurl/shorterurl/controller/UrlShorteningControllerTest.java
+++ src/test/java/com/shorterurl/shorterurl/controller/UrlShorteningControllerTest.java
@@ -10,6 +10,7 @@ import com.shorterurl.shorterurl.service.UrlShorteningService;
 
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -24,6 +25,7 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletResponse;
 
 @ExtendWith(MockitoExtension.class)
 public class UrlShorteningControllerTest {
@@ -84,20 +86,53 @@ public class UrlShorteningControllerTest {
     }
 
     @Test
-void testGetAllUrls() {
-    // Arrange
-    UrlMapping urlMapping1 = new UrlMapping("http://longurlexample.com", "abc123");
-    UrlMapping urlMapping2 = new UrlMapping("http://google.com", "xyz456");
-    List<UrlMapping> urlMappings = new ArrayList<>();
-    urlMappings.add(urlMapping1);
-    urlMappings.add(urlMapping2);
-    Mockito.when(urlShorteningService.getAllUrlMappings()).thenReturn(urlMappings);
+    void testGetAllUrls() {
+        // Arrange
+        UrlMapping urlMapping1 = new UrlMapping("http://longurlexample.com", "abc123");
+        UrlMapping urlMapping2 = new UrlMapping("http://google.com", "xyz456");
+        List<UrlMapping> urlMappings = new ArrayList<>();
+        urlMappings.add(urlMapping1);
+        urlMappings.add(urlMapping2);
+        Mockito.when(urlShorteningService.getAllUrlMappings()).thenReturn(urlMappings);
 
-    // Act
-    ResponseEntity<List<UrlMapping>> response = urlShorteningController.getAllUrls();
+        // Act
+        ResponseEntity<List<UrlMapping>> response = urlShorteningController.getAllUrls();
 
-    // Assert
-    assertEquals(HttpStatus.OK, response.getStatusCode());
-    assertEquals(urlMappings, response.getBody());
-}
+        // Assert
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(urlMappings, response.getBody());
+    }
+    
+    @Test
+    void testRedirectToLongUrl() throws IOException {
+        // Mock the response object
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        // Create a mock URL mapping
+        UrlMapping mockMapping = new UrlMapping();
+        mockMapping.setLongUrl("http://www.example.com");
+
+        // Mock the URL shortening service to return the mock URL mapping
+        UrlShorteningService mockService = Mockito.mock(UrlShorteningService.class);
+        Mockito.when(mockService.getLongUrl(Mockito.anyString())).thenReturn(mockMapping);
+
+        // Create the controller to be tested and call the method
+        UrlShorteningController controller = new UrlShorteningController();
+        controller.setUrlShorteningService(mockService);
+        controller.redirectToLongUrl("abc", response);
+
+        // Assert that the response status is 302 (found) and that the redirect URL is correct
+        assert (response.getStatus() == HttpServletResponse.SC_FOUND);
+        assert (response.getRedirectedUrl().equals(mockMapping.getLongUrl()));
+
+        // Mock the URL shortening service to return null
+        Mockito.when(mockService.getLongUrl(Mockito.anyString())).thenReturn(null);
+
+        // Call the method again with a non-existent short URL
+        response = new MockHttpServletResponse();
+        controller.redirectToLongUrl("xyz", response);
+
+        // Assert that the response status is 404 (not found)
+        assert (response.getStatus() == HttpServletResponse.SC_NOT_FOUND);
+    }
 }
