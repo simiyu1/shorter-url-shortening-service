dbc5db73d545b675b455a0cbfb9acc90c6f0b785
simiyu1 <nabendesimiyu@gmail.com>
2023-04-24 12:03:44 +0300

    fix allow all

--- src/main/java/com/shorterurl/shorterurl/config/WebConfig.java
+++ src/main/java/com/shorterurl/shorterurl/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200",  "http://front.ntoya.link")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
