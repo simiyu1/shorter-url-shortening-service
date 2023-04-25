9fd110e5bdabc8708c620c03a7a258d9b4d92bc6
simiyu1 <nabendesimiyu@gmail.com>
2023-04-25 03:36:52 +0300

    allow CORS

--- src/main/java/com/shorterurl/shorterurl/config/WebConfig.java
+++ src/main/java/com/shorterurl/shorterurl/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOriginPatterns("http://localhost:*","http://front.ntoya.link:*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
